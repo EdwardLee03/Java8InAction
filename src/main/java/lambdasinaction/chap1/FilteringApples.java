90a27ce2e3029155cc638d5b2bfcc926a4e53bcf
guangyi <huagang.lhg@alibaba-inc.com>
2020-06-03 23:38:10 +0800

    Comment "Chapter 1: Java 8: why should you care? / Java 8：你为什么要在乎？"

--- src/main/java/lambdasinaction/chap1/FilteringApples.java
+++ src/main/java/lambdasinaction/chap1/FilteringApples.java
@@ -3,14 +3,18 @@ package lambdasinaction.chap1;
 import java.util.*;
 import java.util.function.Predicate;
 
+/**
+ * Chapter 1: Java 8: why should you care? / Java 8：你为什么要在乎？
+ */
 public class FilteringApples{
 
     public static void main(String ... args){
-
+        // 苹果存货清单
         List<Apple> inventory = Arrays.asList(new Apple(80,"green"),
                                               new Apple(155, "green"),
-                                              new Apple(120, "red"));	
+                                              new Apple(120, "red"));
 
+        // 静态方法引用函数
         // [Apple{color='green', weight=80}, Apple{color='green', weight=155}]
         List<Apple> greenApples = filterApples(inventory, FilteringApples::isGreenApple);
         System.out.println(greenApples);
@@ -18,7 +22,8 @@ public class FilteringApples{
         // [Apple{color='green', weight=155}]
         List<Apple> heavyApples = filterApples(inventory, FilteringApples::isHeavyApple);
         System.out.println(heavyApples);
-        
+
+        // 匿名函数
         // [Apple{color='green', weight=80}, Apple{color='green', weight=155}]
         List<Apple> greenApples2 = filterApples(inventory, (Apple a) -> "green".equals(a.getColor()));
         System.out.println(greenApples2);
@@ -33,8 +38,11 @@ public class FilteringApples{
         System.out.println(weirdApples);
     }
 
+    // JDK 7 之前版本写法
+
     public static List<Apple> filterGreenApples(List<Apple> inventory){
-        List<Apple> result = new ArrayList<>();
+        List<Apple> result = new ArrayList<>(inventory.size());
+        // for-each
         for (Apple apple: inventory){
             if ("green".equals(apple.getColor())) {
                 result.add(apple);
@@ -44,7 +52,7 @@ public class FilteringApples{
     }
 
     public static List<Apple> filterHeavyApples(List<Apple> inventory){
-        List<Apple> result = new ArrayList<>();
+        List<Apple> result = new ArrayList<>(inventory.size());
         for (Apple apple: inventory){
             if (apple.getWeight() > 150) {
                 result.add(apple);
@@ -52,6 +60,8 @@ public class FilteringApples{
         }
         return result;
     }
+
+    // 过滤操作函数
 
     public static boolean isGreenApple(Apple apple) {
         return "green".equals(apple.getColor()); 
@@ -61,8 +71,16 @@ public class FilteringApples{
         return apple.getWeight() > 150;
     }
 
+    // JDK 8 版本写法
+    // 操作函数
+
+    /**
+     * @param inventory 苹果存货清单
+     * @param p 过滤操作函数
+     * @return 期望的苹果列表
+     */
     public static List<Apple> filterApples(List<Apple> inventory, Predicate<Apple> p){
-        List<Apple> result = new ArrayList<>();
+        List<Apple> result = new ArrayList<>(inventory.size());
         for(Apple apple : inventory){
             if(p.test(apple)){
                 result.add(apple);
@@ -72,8 +90,8 @@ public class FilteringApples{
     }       
 
     public static class Apple {
-        private int weight = 0;
-        private String color = "";
+        private final int weight;
+        private final String color;
 
         public Apple(int weight, String color){
             this.weight = weight;
@@ -84,18 +102,11 @@ public class FilteringApples{
             return weight;
         }
 
-        public void setWeight(Integer weight) {
-            this.weight = weight;
-        }
-
         public String getColor() {
             return color;
         }
 
-        public void setColor(String color) {
-            this.color = color;
-        }
-
+        @Override
         public String toString() {
             return "Apple{" +
                    "color='" + color + '\'' +
