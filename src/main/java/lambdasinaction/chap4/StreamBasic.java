327e0cfb6f6ddf13958fc73eb65495724aef22b4
guangyi <bert825_work@163.com>
2020-06-07 22:32:11 +0800

    Comment "Chapter 4: Working with Streams / 数据流操作"

--- src/main/java/lambdasinaction/chap4/StreamBasic.java
+++ src/main/java/lambdasinaction/chap4/StreamBasic.java
@@ -1,50 +1,56 @@
 package lambdasinaction.chap4;
 
-import java.util.*;
-import java.util.stream.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
-import static java.util.Comparator.comparing;
-import static java.util.stream.Collectors.toList;
-
-import static lambdasinaction.chap4.Dish.menu;
-
+/**
+ * Chapter 4: Working with Streams / 数据流操作
+ */
 public class StreamBasic {
 
-    public static void main(String...args){
+    public static void main(String... args) {
+        // 实例方法函数对象
         // Java 7
-        getLowCaloricDishesNamesInJava7(Dish.menu).forEach(System.out::println);
+        getLowCaloricDishesNamesInJava7(Dish.menu)
+                .forEach(System.out::println);
 
         System.out.println("---");
 
         // Java 8
-        getLowCaloricDishesNamesInJava8(Dish.menu).forEach(System.out::println);
+        getLowCaloricDishesNamesInJava8(Dish.menu)
+                .forEach(System.out::println);
 
     }
 
-    public static List<String> getLowCaloricDishesNamesInJava7(List<Dish> dishes){
-        List<Dish> lowCaloricDishes = new ArrayList<>();
-        for(Dish d: dishes){
-            if(d.getCalories() < 400){
+    public static List<String> getLowCaloricDishesNamesInJava7(List<Dish> dishes) {
+        List<Dish> lowCaloricDishes = new ArrayList<>(dishes.size());
+        // for-each
+        for (Dish d : dishes) {
+            if (d.getCalories() < 400) {
                 lowCaloricDishes.add(d);
             }
         }
-        List<String> lowCaloricDishesName = new ArrayList<>();
+        List<String> lowCaloricDishesName = new ArrayList<>(lowCaloricDishes.size());
+        // lowCaloricDishes.sort((d1, d2) -> Integer.compare(d1.getCalories(), d2.getCalories()))
         Collections.sort(lowCaloricDishes, new Comparator<Dish>() {
-            public int compare(Dish d1, Dish d2){
+            public int compare(Dish d1, Dish d2) {
                 return Integer.compare(d1.getCalories(), d2.getCalories());
             }
         });
-        for(Dish d: lowCaloricDishes){
+        for (Dish d : lowCaloricDishes) {
             lowCaloricDishesName.add(d.getName());
         }
         return lowCaloricDishesName;
     }
 
-    public static List<String> getLowCaloricDishesNamesInJava8(List<Dish> dishes){
+    public static List<String> getLowCaloricDishesNamesInJava8(List<Dish> dishes) {
         return dishes.stream()
                 .filter(d -> d.getCalories() < 400)
-                .sorted(comparing(Dish::getCalories))
+                .sorted(Comparator.comparing(Dish::getCalories))
                 .map(Dish::getName)
-                .collect(toList());
+                .collect(Collectors.toList());
     }
 }
